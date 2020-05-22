e6570556ab57eade542f7607f7630cf732f94718
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-05-22 13:55:12 +0200

    :art: Improve Code Format

--- R/zzz.R
+++ R/zzz.R
@@ -192,6 +192,135 @@ rm_brackets <- function(x) {
 }
 
 
+get_torrent <- function(imdb_id, path = ".") {
+
+  if (missing(imdb_id)) {
+    stop("Argument 'imdb_id' is required.")
+  }
+
+  if (is.null(imdb_id)) {
+    stop("Argument 'imdb_id' cannot be NULL.")
+  }
+
+  if (length(imdb_id) != 1) {
+    stop("Argument 'imdb_id' must be a character of length 1.")
+  }
+
+  if (!is.character(imdb_id)) {
+    stop("Argument 'imdb_id' must be a character of length 1.")
+  }
+
+  if (!sum(grep("^tt[0-9]{7}$", imdb_id))) {
+    stop("Invalid 'imdb_id' format.")
+  }
+
+  if (!dir.exists(path)) {
+    stop(paste("Directory <", path, "> does not exist."))
+  }
+
+  dir.create(file.path(path, "torrents"), showWarnings = FALSE)
+
+  request  <- yts_full_url(
+    "list_movies.json",
+    "?query_term=",
+    imdb_id
+  )
+
+
+  ## YTS API Communication ----
+
+  response <- send_request(request)
+
+  content  <- parse_response(response, api = "yts")
+
+
+  ## Download Movie Cover ----
+
+  attempt <- NULL
+
+  if (length(content)) {
+
+    torrents <- content$data$movies$torrents
+    torrents <- do.call(rbind.data.frame, torrents)
+    rownames(torrents) <- NULL
+
+    if (nrow(torrents)) {
+
+      print(torrents[ , c("quality", "type", "seeds", "peers", "size")])
+
+      number <- readline("\n  Which one would you like? ")
+
+      if (number %in% (1:nrow(torrents))) {
+
+        torrent <- torrents[number, ]
+        usethis::ui_info(
+          paste(
+            "You have selected:",
+            usethis::ui_value(
+              paste0(torrent[1, "quality"], " (", torrent[1, "type"], ")")
+            ),
+            "with",
+            usethis::ui_value(torrent[1, "seeds"]), "(seeds)",
+            "and",
+            usethis::ui_value(torrent[1, "peers"]), "(peers)",
+            "for a total size of",
+            usethis::ui_value(torrent[1, "size"])
+          )
+        )
+
+        attempt <- tryCatch({
+          utils::download.file(
+            url      = torrent$"url",
+            destfile = file.path(path, "torrents", paste0(imdb_id, ".torrent")),
+            quiet    = TRUE
+          )},
+          error = function(e){}
+        )
+
+        if (is.null(attempt)) {
+
+          usethis::ui_oops("Unable to download the torrent file.")
+
+        } else {
+
+          usethis::ui_done(
+            paste(
+              "Torrent file has been successfully stored in",
+              usethis::ui_value(
+                file.path(path, "torrents", paste0(imdb_id, ".torrent"))
+              )
+            )
+          )
+        }
+
+      } else {
+
+        torrent <- data.frame()
+        usethis::ui_info("No torrent selected.")
+      }
+
+    } else {
+
+      usethis::ui_oops(
+        paste(
+          "No torrent found for",
+          usethis::ui_value(imdb_id)
+        )
+      )
+    }
+
+  } else {
+
+    usethis::ui_oops(
+      paste(
+        "No torrent found for",
+        usethis::ui_value(imdb_id)
+      )
+    )
+  }
+}
+
+
 yml_to_list <- function(filename) {
 
   data <- yaml::read_yaml(filename)
